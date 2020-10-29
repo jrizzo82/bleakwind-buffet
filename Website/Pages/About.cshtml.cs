9d0b5c2c185e30695f631f1b74641856c8f4873b
Jerry Rizzo <jrizzo@ksu.edu>
2020-10-28 19:07:00 -0500

    website milestone 1 complete

---
+++ Website/Pages/About.cshtml.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Website.Pages
+{
+    public class AboutModel : PageModel
+    { 
+    
+        public string Title
+        {
+            get => "About";
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
